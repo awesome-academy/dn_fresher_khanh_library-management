a2d1f0c329b35c17b9e837686481bf21e894f383
khanhpn-2944 <pham.nhat.khanh@sun-asterisk.com>
2022-03-31 15:30:53 +0700

    pnkhanh 20220331 1530: User sign in and sign out

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_050844) do
+ActiveRecord::Schema.define(version: 2022_03_31_044422) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2022_03_29_050844) do
     t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email"
   end
 
