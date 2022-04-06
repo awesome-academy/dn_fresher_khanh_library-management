2bca3fa1ab575a95f078df02a84d8ffc7105862a
khanhpn-2944 <pham.nhat.khanh@sun-asterisk.com>
2022-04-06 13:47:14 +0700

    pnkhanh 20220406 1347: Admin list requests

--- app/models/borrowed_request.rb
+++ app/models/borrowed_request.rb
@@ -3,6 +3,10 @@ class BorrowedRequest < ApplicationRecord
 
   belongs_to :user
 
+  scope :order_status, ->{order :status, created_at: :desc}
+
+  delegate :name, to: :user, prefix: true
+
   validates :status, numericality: {only_integer: true}
   enum status: {requested: 0, borrowing: 1, returned: 2, rejected: 3}
 end
